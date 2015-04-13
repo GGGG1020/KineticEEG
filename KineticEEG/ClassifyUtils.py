9a2fca0b8c63d2bb059f3c48255c159798042b5e
GGGG1020 <gauravrghosal@gmail.com>
2015-04-12 17:30:24 -0700

    Added list_d function
    list_d is a more helpful distance finding function for this project.

--- KineticEEG/ClassifyUtils.py
+++ KineticEEG/ClassifyUtils.py
@@ -5,5 +5,9 @@ def euclideandistance(inst1, inst2, leng):
     d=0
     for i in range(leng):
         d+=pow((inst1[i]-inst2[i]),2)
-
     return math.sqrt(d)
+def list_d(inst1, inst2, leng):
+    d_list=list()
+    for i in range(leng):
+        d_list.append(int(abs(inst1[i]-inst2[i])))
+    return d_list
