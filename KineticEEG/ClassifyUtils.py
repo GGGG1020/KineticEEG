bf0b305bebd9e1f5eab8c184638b8f6cd8171529
GGGG1020 <gauravrghosal@gmail.com>
2015-04-13 10:35:43 -0700

    Updated trainingdata function
    This is to  have a more  simple way to work with training data in the
    program.

--- KineticEEG/ClassifyUtils.py
+++ KineticEEG/ClassifyUtils.py
@@ -12,10 +12,18 @@ def list_d(inst1, inst2, leng):
         d_list.append(int(abs(inst1[i]-inst2[i])))
     return d_list
 def load_trainingdata(file):
+    """The training data file will look like this:\
+    TRAINING.dat
+    Fc4, 3.39, 1.00,1.89,0.00, arm
+    The dictionary returned will look like this:
+    trainingdat={"FC4":[[3.39, 1.00,1.89,0.00, "arm"],[6.34,0.879,1.23,0.10, "kick"]]}"""
     f=open(file, "r")
-    trainingdat=list()
+    trainingdat=dict()
     for i in f.readlines():
-        trainingdat.append(i)
+        i=i.split()
+        senor=i.pop(0)
+        trainingdat[sensor].append(i)
+        
     return trainingdat
 
         
