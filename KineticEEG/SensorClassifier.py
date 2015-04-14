a4ec7a232692560a8bb60b288e2b920aa247543a
GGGG1020 <gauravrghosal@gmail.com>
2015-04-14 11:57:02 -0700

    Real Situation Tests

--- KineticEEG/SensorClassifier.py
+++ KineticEEG/SensorClassifier.py
@@ -7,23 +7,25 @@ trainingdata is a list of lists
 class SensorClassifier:
     """This classifies the sensor's information in a binary form"""
     def __init__(self,sensor,sensordata, trainingdata):
+        "Initializer!"
         self. sensordatalegnth=len(sensordata)
-        self.sensordata=sensordata[sensor]
-        self.trainingdata=trainingdata
-        self.delta_val=self.sensordata[0]
-        self.theta_val=self.sensordata[1]
-        self.alpha_val=self.sensordata[2]
-        self.beta_val=self.sensordata[3]
-    def get_neighbors(self, inst, num):
-        ds=list()
+        self.trainingdata=trainingdata[sensor]
+        self.sensordata=sensordata
+        self.delta_val=float(self.sensordata[0])
+        self.theta_val=float(self.sensordata[1])
+        self.alpha_val=float(self.sensordata[2])
+        self.beta_val=float(self.sensordata[3])
+        self.inst=list([self.delta_val, self.theta_val, self.alpha_val, self.beta_val])
+    def get_neighbors(self,num):
+        self.ds=list()
         leng=len(self.trainingdata)-1
         for i in range(len(self.trainingdata)):
-            distance=ClassifyUtils.euclideandistance((inst, self.trainingdata[i],leng))
-            ds.append(self.trainingdata[i], distance)
-        ds.sort(key=lambda tup:tup[1])
+            distance=ClassifyUtils.euclideandistance(self.inst, self.trainingdata[i],leng)
+            self.ds.append((self.trainingdata[i], distance))
+        self.ds.sort(key=lambda tup:tup[1])
         self.neighbors=list()
         for i in range(num):
-            self.neighbors.append(ds[x][0])
+            self.neighbors.append(self.ds[i][0])
     def get_responses(self):
         self.votes=dict()
         for i in range(len(self.neighbors)):
