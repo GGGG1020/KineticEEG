4a9b99c2666d458c337446cd4ec79e1defa6b7b7
GGGG1020 <gauravrghosal@gmail.com>
2015-04-14 00:03:49 -0700

    Beginning implementation of the vote-getter
    This is to get votes of neighbors!

--- KineticEEG/SensorClassifier.py
+++ KineticEEG/SensorClassifier.py
@@ -4,9 +4,6 @@ The list will be like this:
 >>>data=[delta, theta, alpha, beta, "class"]
 trainingdata is a list of lists
 """
-
-
-
 class SensorClassifier:
     """This classifies the sensor's information in a binary form"""
     def __init__(self,sensor,sensordata, trainingdata):
@@ -17,17 +14,22 @@ class SensorClassifier:
         self.theta_val=self.sensordata[1]
         self.alpha_val=self.sensordata[2]
         self.beta_val=self.sensordata[3]
-    def get_neighbors(self, allneighbors, inst, num):
+    def get_neighbors(self, inst, num):
         ds=list()
-        leng=len(allneighbors)-1
-        for i in range(len(allneighbors)):
-            distance=ClassifyUtils.euclideandistance((inst, allneighbors[i],leng))
-            ds.append(allneighbors[i], distance)
+        leng=len(self.trainingdata)-1
+        for i in range(len(self.trainingdata)):
+            distance=ClassifyUtils.euclideandistance((inst, self.trainingdata[i],leng))
+            ds.append(self.trainingdata[i], distance)
         ds.sort(key=lambda tup:tup[1])
-        neighbors=list()
+        self.neighbors=list()
         for i in range(num):
-            neighbors.append(ds[x][0])
-        return neighbors
+            self.neighbors.append(ds[x][0])
+    def get_responses(self):
+        self.votes=dict()
+        for i in range(len(self.neighbors)):
+            
+        
+    
         
     
         
