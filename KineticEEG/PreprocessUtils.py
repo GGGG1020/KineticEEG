f690eac41f38458d88a49055dc67f16a21d9a78c
GGGG1020 <gauravrghosal@gmail.com>
2015-04-15 13:21:58 -0700

    Added basic_window()
    An even more basic windowing function suitable for emotiv.

--- KineticEEG/PreprocessUtils.py
+++ KineticEEG/PreprocessUtils.py
@@ -11,4 +11,13 @@ def apply_hamming_window(data, samples):
 def get_magnitude(complex_num):
     magnitude=math.sqrt(complex_num.real*complex_num.real+complex_num.imag*complex_num.imag)
     return magnitude
+def basic_window(data):
+    lendata=len(data)
+    g=lendata/128
+    x=numpy.linspace(0,g,lendata)
+    A=numpy.sin(numpy.pi*x/g)*numpy.sin(numpy.pi*x/g)
+    data=A.T*data
+    return data
+    
 
+	
