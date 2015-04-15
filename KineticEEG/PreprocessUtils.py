3f84bba11e25a2e95a6dccd0601227b51dbb9096
GGGG1020 <gauravrghosal@gmail.com>
2015-04-15 11:53:56 -0700

    Added get_magnitude()
    This gets the magnitude of the complex number!

--- KineticEEG/PreprocessUtils.py
+++ KineticEEG/PreprocessUtils.py
@@ -3,10 +3,12 @@ This includes the basic utilities and helper functions for pre-processing the EE
 
 import numpy
 from numpy import fft as fft
-
-
-
+import math
 def apply_hamming_window(data, samples):
     window=numpy.hamming(samples)
     data=data*window
     return data
+def get_magnitude(complex_num):
+    magnitude=math.sqrt(complex_num.real*complex_num.real+complex_num.imag*complex_num.imag)
+    return magnitude
+
