fa54b7f70aa1bdb254f6e2657411efb5c1350827
GGGG1020 <gauravrghosal@gmail.com>
2015-04-16 11:52:52 -0700

    Added bin_power()
    This finds power in the bands.

--- KineticEEG/PreprocessUtils.py
+++ KineticEEG/PreprocessUtils.py
@@ -4,7 +4,7 @@ This includes the basic utilities and helper functions for pre-processing the EE
 import numpy
 from numpy import fft as fft
 import math
-import scipy
+import scipy.signal
 def apply_hamming_window(data, samples):
     window=numpy.hamming(samples)
     data=data*window
@@ -25,6 +25,16 @@ def butter_highpass_filter(data, cutoff, samplingfreq, order):
     b,a = scipy.signal.butter(order, norm_cutoff,'highpass')
     filtereddat=scipy.signal.lfilter(b,a,data)
     return filtereddat
+def bin_power(X,Band,Fs):
+	C = fft.fft(X)
+	C = abs(C)
+	Power =numpy.zeros(len(Band)-1);
+	for Freq_Index in range(0,len(Band)-1):
+		Freq = float(Band[Freq_Index])										
+		Next_Freq = float(Band[Freq_Index+1])
+		Power[Freq_Index] = sum(C[numpy.floor(Freq/Fs*len(X)):numpy.floor(Next_Freq/Fs*len(X))])
+	Power_Ratio = Power/sum(Power)
+	return Power, Power_Ratio	
     
 
 	
