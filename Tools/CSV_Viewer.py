f81c795daceb498ec88e4f0069b0cf730e39161e
GGGG1020 <gauravrghosal@gmail.com>
2015-04-21 18:50:56 -0700

    Added CSV_Viewer
    This is a helpful dev tool to find patterns

---
+++ Tools/CSV_Viewer.py
@@ -0,0 +1,38 @@
+import matplotlib
+matplotlib.use('TKAgg')
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+sys.path.append("C:/Users/Gaurav/My Documents/GitHub/KineticEEG/KineticEEG")
+sys.path.append("C:/Users/Gaurav/My Documents/GitHub/KineticEEG/Tools")
+import CSV_Extractor
+import Preprocessers
+import PreprocessUtils
+"""This is a command line app to view fft"""
+def animated_barplot(file):
+    # http://www.scipy.org/Cookbook/Matplotlib/Animations
+    a=CSV_Extractor.CSVExtractor(file)
+    b=a.get_data_from_sensor("FC5", 2048)
+    chunk0=0
+    chunk1=1024
+    rects = plt.bar(range(1), 0,  align = 'center')
+    for i in range(1024):
+        dat=b[chunk0:chunk1]
+        r=PreprocessUtils.butter_highpass_filter(dat,0.16,128,5)
+        c=PreprocessUtils.basic_window(r)
+        er=PreprocessUtils.bin_power(c, [1,4,7,13,30], 128)
+        for rect in rects:
+            print(er[1][0])
+            rect.set_height(er[1][0])
+        chunk0+=1
+        chunk1+=1
+        fig.canvas.draw()
+file=sys.argv[1]
+fig = plt.figure()
+plt.xlim(0,9)
+plt.ylim(0,0.2)
+win = fig.canvas.manager.window
+plt.ion()
+win.after(100, lambda:animated_barplot(file))
+plt.show()
+
