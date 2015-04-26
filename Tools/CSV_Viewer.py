30200b9ff41edbc8cf1e134311424956ffdf6d55
GGGG1020 <gauravrghosal@gmail.com>
2015-04-26 13:25:36 -0700

    Made CSV_Viewer more customizable and added Docstring

--- Tools/CSV_Viewer.py
+++ Tools/CSV_Viewer.py
@@ -1,4 +1,5 @@
 import matplotlib
+import os
 matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,8 +9,24 @@ sys.path.append("C:/Users/Gaurav/My Documents/GitHub/KineticEEG/Tools")
 import CSV_Extractor
 import Preprocessers
 import PreprocessUtils
-"""This is a command line app to view fft"""
-def animated_barplot(file):
+"""CSV_Viewer
+Written by Gaurav Ghosal as a tool for KineticEEG
+______________________________
+This is a commandline program to help view and fft the csv files"""
+"""USAGE:
+_______________________
+python CSV_Viewer.py path_to_csv.csv step ymin ymax
+
+DESCRIPTION OF ARGUMENTS
+___________________________
+path_to_csv is the path to the csv that you want to display
+
+step is the number of elapsed samples between ffts()
+
+ymin is the minimum y value on the graph
+
+ymax is the maximum y value on the graph"""
+def animated_barplot(file,step):
     # http://www.scipy.org/Cookbook/Matplotlib/Animations
     a=CSV_Extractor.CSVExtractor(file)
     b=a.get_data_from_sensor("FC5", 8192)
@@ -24,16 +41,38 @@ def animated_barplot(file):
         for rect in rects:
             print(er[1][0])
             rect.set_height(er[1][0])
-        chunk0+=1
-        chunk1+=1
+        chunk0+=step
+        chunk1+=step
         fig.canvas.draw()
 file=sys.argv[1]
+if not os.exists(file):
+    print("""CSV_Viewer
+Written by Gaurav Ghosal as a tool for KineticEEG
+______________________________
+This is a commandline program to help view and fft the csv files""")
+    print("""USAGE:
+_______________________
+python CSV_Viewer.py path_to_csv.csv step ymin ymax
+
+DESCRIPTION OF ARGUMENTS
+___________________________
+path_to_csv is the path to the csv that you want to display
+
+step is the number of elapsed samples between ffts()
+
+ymin is the minimum y value on the graph
+
+ymax is the maximum y value on the graph""")
+step=int(sys.argv[2])
+ymin=int(sys.argv[3])
+ymax=int(sys.argv[4])
 print(sys.argv[1])
 fig = plt.figure()
 plt.xlim(0,9)
-plt.ylim(0, 2)
+plt.ylim(ymin, ymax)
 win = fig.canvas.manager.window
 plt.ion()
-win.after(100, lambda:animated_barplot(file))
+win.after(100, lambda:animated_barplot(file,step))
 plt.show()
+
 
