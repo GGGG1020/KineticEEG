a5938a3f14709b31a34cbce8da70ad8aa388a1f3
GGGG1020 <gauravrghosal@gmail.com>
2015-05-03 09:08:15 -0700

    Reimplemented DataProcessor class

--- KineticEEG/Preprocessers.py
+++ KineticEEG/Preprocessers.py
@@ -14,8 +14,8 @@ The array for the each contains the uV data."""
 class DataProcessor:
     """This does all the data processing in it's stages:
     High Pass Filter(0.16Hz.)->Windowing Function->FFT->Convert to Power for Bands"""
-    def __init__(self, arrays):
-        self.data_dict={"AF3":arrays[0], "F7":arrays[1],"F3":arrays[2],"FC5":arrays[3],"T7":arrays[4],"P7":arrays[5],"O1":arrays[6],"O2":arrays[7],"P8":arrays[8],"T8":arrays[9],"FC6":arrays[10],"F4":arrays[11],"AF4":arrays[12]}
+    def __init__(self):
+        self.data_dict=dict()
     def do_high_pass(self):
         for item in self.data_dict.keys():
             self.data_dict[item]=PreprocessUtils.butter_highpass_filter(self.data_dict[item],0.16,128,5)
@@ -25,8 +25,9 @@ class DataProcessor:
     def do_bin_power(self):
         for item in self.data_dict.keys():
             self.data_dict[item]=PreprocessUtils.bin_power(self.data_dict[item], [1,4,7,13,30], 128)
-
-            
+    def update_data(self, dictofdat):
+        self.data_dict.clear()
+        self.data_dict=dictofdat            
         
         
         
