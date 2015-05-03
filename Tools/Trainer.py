3fc6210f895d3ac4aa50b0f85d88259516f78716
GGGG1020 <gauravrghosal@gmail.com>
2015-05-03 07:19:51 -0700

    Created GUI for Trainer
    This will be easier to use than a command-line application

--- Tools/Trainer.py
+++ Tools/Trainer.py
@@ -6,12 +6,19 @@ class TrainerApp(tkinter.Tk):
     def __init__(self, parent):
         tkinter.Tk.__init__(self, parent)
         self.parent=parent
-        self.minsize(width=666, height=666)
+        self.minsize(width=400, height=400)
+        self.maxsize(width=400, height=400)
         self.initialize()
     def initialize(self):
-        self.grid()
         self.closebtn=tk.Button(self, text="close")
-        self.closebtn.grid(column=10, row=3, )
+        self.closebtn.place(x=325,y=0)
+        self.train_kickbtn=tk.Button(self, text="Train Kick")
+        self.train_kickbtn.place(x=120, y=100)
+        self.train_armbtn=tk.Button(self, text="Train Arm")
+        self.train_armbtn.place(x=200, y=100)
+        self.train_nuetralbtn=tk.Button(self, text="Train Nuetral")
+        self.train_nuetralbtn.place(x=160, y=130)  
 if __name__=='__main__':
     app=TrainerApp(None)
     app.title("Trainer")
+    app.mainloop()
