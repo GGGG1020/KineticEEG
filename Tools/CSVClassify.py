e04cbfab510c49c39a70442ab41cc82a32d58f9b
Gaurav Ghosal <gauravrghosal@gmail.com>
2015-06-16 17:28:09 -0700

    Work on CSV based classifier

--- Tools/CSVClassify.py
+++ Tools/CSVClassify.py
@@ -9,6 +9,18 @@ class CSV_Classifier:
     def __init__(self, file):
         self.extract=CSV_Extractor.CSVExtractor(file)
         self.processor=Preprocessers.DataProcessor()
+        self.listolist=dict()
+    def getwindow(self, sizeofwindow):
+        for i in self.extract.sensor2column:
+            graf=self.extract.get_data_from_sensor(i, sizeofwindow)
+            self.listolist[i]=graf
+    def preprocess(self):
+        self.processor.update_data(self.listolist)
+        self.processor.do_highpass()
+        self.processor.do_hanning_wndow()
+        self.processor.do_bin_power()
+        self.processor.
+        
 class CSV_Trainer:
     """Gathers the nessecary data for doing the classification"""
     def __init__(self):pass
