401b7dbbea7404e34b6c9c318c3b16b1203a2a94
Gaurav Ghosal <gauravrghosal@gmail.com>
2015-06-22 18:09:30 -0700

    New Class Variable

--- Tools/CSV_Extractor.py
+++ Tools/CSV_Extractor.py
@@ -4,34 +4,48 @@ class CSVExtractor:
     def __init__(self, file):
         self.sensor2column={"AF3":2, "F7":3, "F3":4,"FC5":5, "T7":6,"P7":7,"O1":8,"O2":9,"P8":10, "T8":11, "FC6":12,"F4":13,"F8":14,"AF4":15}
         self.fileob=open(file, "r")
+        self.file=file
+        self.nicethingy=dict({'F3': [], 'FC5': [], 'T7': [], 'F7': [], 'P7': [], 'P8': [], 'AF4': [], 'O2': [], 'O1': [], 'T8': [], 'AF3': [], 'FC6': [], 'F4': [], 'F8': []})
         self.csv_read=csv.reader(self.fileob)
         self.large_list=list()
+        indx=int(0)
         for i in self.csv_read:
             self.large_list.append(i)
         print(len(self.large_list))
         if self.large_list[0][0][0]=="t":
             del self.large_list[0]
-            
-##    def get_data_from_sensor(self, sensor, length):
-##        indx=self.sensor2column[sensor]
-##        list_of_vals=list()
-##        for i in range(length+1):
-##            curr_line=next(self.csv_read)
-##            if curr_line[0][0]=="t":
-##                pass
-##            else:
-##                list_of_vals.append(float(curr_line[indx]))
-##        return list_of_vals
+        self.lennylen=len(self.large_list)
+        #Now be nice to our friend at 0x058A18B0
+        for i in self.large_list:
+            for j in self.sensor2column:
+                self.nicethingy[j].append(self.large_list[indx][self.sensor2column[j]])
+            indx+=1
+    def get_data_from_sensor(self, sensor, length):
+        newfileob=open(self.file,"r")
+        csv_read=csv.reader(newfileob)
+        indx=self.sensor2column[sensor]
+        list_of_vals=list()
+        for i in range(length+1):
+            ##print(i)
+            curr_line=next(csv_read)
+            if curr_line[0][0]=="t":
+                pass
+            else:
+                list_of_vals.append(float(curr_line[indx]))
+        return list_of_vals
     def get128more(self,sensor):
         indx=self.sensor2column[sensor]
         listylist=list()
         for i in range(128):
-            listylist.append(float(self.large_list[i][indx]))
-            del self.large_list[i][indx]
+            listylist.append(self.nicethingy[sensor][0])
+            del self.nicethingy[sensor][0]
+        return listylist 
+    def get16more(self, sensor):
+        listylist=list()
+        for i in range(16):
+            listylist.append(self.nicethingy[sensor][0])
+            del self.nicethingy[sensor][0]
         return listylist
-
-        
-        
     
         
                         
--- Tools/CSV_Viewer.py
+++ Tools/CSV_Viewer.py
@@ -1,6 +1,7 @@
 import matplotlib
 import os.path
-matplotlib.use('TKAgg')
+import time
+#matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -28,10 +29,12 @@ ymin is the minimum y value on the graph
 ymax is the maximum y value on the graph"""
 def animated_barplot(file,step,how_muc,size):
     # http://www.scipy.org/Cookbook/Matplotlib/Animations
+    print("l")
     a=CSV_Extractor.CSVExtractor(file)
     b=a.get_data_from_sensor("FC5", how_much)
     chunk0=0
     chunk1=size
+    print("G")
     rects = plt.bar(range(1), 0,  align = 'center')
     rect1=plt.bar(2, 0,  align = 'center')
     rect2=plt.bar(3, 0,  align = 'center')
@@ -45,7 +48,7 @@ def animated_barplot(file,step,how_muc,size):
         er=PreprocessUtils.bin_power(c, [1,4,7,13,30], 128)
         j.append(er[1][0])
         for rect in rects:
-            print(er[1][0])
+            #print(er[1][0])
             rect.set_height(er[1][0])
         for rect in rect1:
             rect.set_height(er[1][1])
@@ -56,20 +59,58 @@ def animated_barplot(file,step,how_muc,size):
         chunk0+=step
         chunk1+=step
         fig.canvas.draw()
-def rollingbarplot(file, step, size):
+        time.sleep(0.0625)
+
+def rollingbarplot(file, step, size,sensor):
     a=CSV_Extractor.CSVExtractor(file)
-    proc=Processers.DataProcessor()
-    temproc=Processers.DataProcessor()
+    proc=Preprocessers.DataProcessor()
+    temproc=Preprocessers.DataProcessor()
+    rects = plt.bar(range(1), 0,  align = 'center')
+    rect1=plt.bar(2, 0,  align = 'center')
+    rect2=plt.bar(3, 0,  align = 'center')
+    rect3=plt.bar(4, 0,  align = 'center')
     tree={'F3': [], 'FC5': [], 'T7': [], 'F7': [], 'P7': [], 'P8': [], 'AF4': [], 'O2': [], 'O1': [], 'T8': [], 'AF3': [], 'FC6': [], 'F4': [], 'F8': []}
     step=False
-    while True:
+    binnedstuff=dict()
+    while not len(tree["F3"])==size:
         for i in a.sensor2column:
-            fed=a.get256more(i)
+            fed=a.get128more(i)
+            fed={i:fed}
+            PreprocessUtils.
             temproc.update_data(fed)
-            temproc.do_highpass()
+            temproc.do_high_pass()
             fed=temproc.data_dict
             tree[i]+=fed
+    while True:
         if len(tree[0])==size:
+                #Now it is time to actually do the calculations
+            proc.update_data(tree)
+            proc.do_hann_wndow()
+            proc.do_bin_power()
+            binnedstuff=proc.data_dict
+            interestingbinnedstuff=proc.data_dict[sensor][1]
+        else:break
+        for rect in rects:
+            #print(er[1][0])
+            rect.set_height(interestingbinnedstuff[0])
+        for rect in rect1:
+            rect.set_height(interestingbinnedstuff[1])
+        for rect in rect2:
+            rect.set_height(interestingbinnedstuff[2])
+        for rect in rect3:
+            rect.set_height(interestingbinnedstuff[3])
+        for i in tree:
+            del tree[i][0:16]            
+        time.sleep(16/128)
+        for i in tree:
+            tree[i]+=a.get16more(i)
+            
+            
+            
+        
+   
+            
+#run 8 times per second            
             
 
 file=sys.argv[1]
@@ -79,21 +120,15 @@ Written by Gaurav Ghosal as a tool for KineticEEG
 ______________________________
 This is a commandline program to help view and fft the csv files""")
     print("""USAGE:
-_______________________
+______________________
 python CSV_Viewer.py path_to_csv.csv step ymin ymax tofetch
-
 DESCRIPTION OF ARGUMENTS
 ___________________________
 path_to_csv is the path to the csv that you want to display
-
 step is the number of elapsed samples between ffts()
-
 ymin is the minimum y value on the graph
-
 ymax is the maximum y value on the graph
-
 tofetch is how much data to get
-
 size is size of fft""")
     exit()
 step=int(sys.argv[2])
@@ -101,6 +136,7 @@ ymin=int(sys.argv[3])
 ymax=int(sys.argv[4])
 how_much=int(sys.argv[5])
 size=int(sys.argv[6])
+sensor=str(sys.argv[7])
 print(sys.argv[1])
 fig = plt.figure()
 plt.xlim(0,9)
@@ -108,7 +144,9 @@ plt.ylim(ymin, ymax)
 win = fig.canvas.manager.window
 plt.ion()
 j=list()
-win.after(100, lambda:animated_barplot(file,step,how_much,size))
-plt.show()
+print("Hi")
+#Program isn't making it here
+win.after(0, lambda:rollingbarplot(file,step,size, sensor))
 
+plt.show()
 
