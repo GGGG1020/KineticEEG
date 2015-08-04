7c562175b6a043bbca04d81ede503963b9ba1079
Gaurav Ghosal <gauravrghosal@gmail.com>
2015-08-04 16:48:57 -0700

    Continued work on preprocesser

--- KineticEEG/CSVTrainer.py
+++ KineticEEG/CSVTrainer.py
@@ -16,7 +16,16 @@ class CSV_Trainer:
         self.rawspecdict=dict()
         self.proc_specdict=dict()
     def preprocess(self, dictth):
+        dictt1=dict()
         for i in dictth.keys():
+            nowconsd=dictth[i]
+            tree=PreprocessUtils.highpass(nowconsd)
+            tree1=numpy.hanning(1024)*tree
+            stuff2=PreprocessUtils.bin_power(stuff1, [1,4,7,13,30], 128)
+            stuff4=abs(20*numpy.log(stuff2))
+            stuff5=tuple(stuff4[0])
+            dictt1[i]=[stuff5]
+        return dictt1
             
             
     def train_action(self, action,time1, time2):
@@ -25,6 +34,7 @@ class CSV_Trainer:
         startpos,endpos=time1*128, time2*128
         for x in SENSORS_OF_INTEREST:
             self.rawspecdict[x]=self.csv_extract.get_with_constraint(x, startpos, endpos)
+        
             
         
         
