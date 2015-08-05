49a9157f1c5094e106db8efbdeb06826f553411f
Gaurav Ghosal <gauravrghosal@gmail.com>
2015-08-05 15:15:27 -0700

    Added more to this

--- KineticEEG/CSVTrainer.py
+++ KineticEEG/CSVTrainer.py
@@ -5,35 +5,42 @@ sys.path.append("C:/Users/Gaurav/Documents/GitHub/KineticEEG/Tools")
 import CSV_Extractor
 import PreprocessUtils
 import numpy
-
+import time
 class CSV_Trainer:
     """This trains with CSV"""
-    SENSORS_OF_INTEREST=["FC5","F3","F4","FC6"]
     def __init__(self, file):
         ##self.fileobj=open(file, mode='r')
         self.file_name=str(file)
         self.csvextract=CSV_Extractor.CSVExtractor(self.file_name)
+        self.SENSORS_OF_INTEREST=["FC5","F3","F4","FC6"]
         self.rawspecdict=dict()
         self.proc_specdict=dict()
-    def preprocess(self, dictth):
-        dictt1=dict()
+    def preprocess(self, dictth,dictt1):
         for i in dictth.keys():
             nowconsd=dictth[i]
             tree=PreprocessUtils.highpass(nowconsd)
             tree1=numpy.hanning(1024)*tree
-            stuff2=PreprocessUtils.bin_power(stuff1, [1,4,7,13,30], 128)
+            stuff2=PreprocessUtils.bin_power(tree1, [1,4,7,13,30], 128)
             stuff4=abs(20*numpy.log(stuff2))
             stuff5=tuple(stuff4[0])
-            dictt1[i]=[stuff5]
+            dictt1[i].append(stuff5[0])
+        return dictt1            
+    def train_action(self, action,time1):
+        """time1 and time2 seconds"""
+        assert action=='kick',action=='arm'
+        startpos=time1*128
+        endpos=startpos+1024
+        dictt1=dict()
+        for i in self.SENSORS_OF_INTEREST:
+            dictt1[i]=list()
+        while True:
+            for x in self.SENSORS_OF_INTEREST:
+                self.rawspecdict[x]=self.csvextract.get_with_constraints(x, startpos, endpos)
+            dictt1=self.preprocess(self.rawspecdict, dictt1)
+            startpos+=16; endpos+=16
         return dictt1
             
             
-    def train_action(self, action,time1, time2):
-        """time1 and time2 seconds"""
-        assert action=='kick',action=='arm'
-        startpos,endpos=time1*128, time2*128
-        for x in SENSORS_OF_INTEREST:
-            self.rawspecdict[x]=self.csv_extract.get_with_constraint(x, startpos, endpos)
         
             
         
