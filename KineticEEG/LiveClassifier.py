0c27dbd1bed3b50ce4057337e6d00796d6df7508
Gaurav Ghosal <gauravrghosal@gmail.com>
2016-06-16 23:22:36 -0700

    Added Multiprocessing for multi-movement classifier
    No help :-1:

--- KineticEEG/LiveClassifier.py
+++ KineticEEG/LiveClassifier.py
@@ -57,7 +57,7 @@ class MultiLiveTrainingDataGatherer:
                         first=False
                     for i in data_dict[tp]:
                         data_dict[tp][i].append(data[i][0][2])
-                    if len(data_dict[tp]["F3"])==16:
+                    if len(data_dict[tp]["F3"])==32:
                         for i in data_dict[tp]:
                             del data_dict[tp][i][0]
                         #print(self.livedataclass.test_classifiers_ret(data_dict))
@@ -100,12 +100,12 @@ class LiveTrainingDataGatherer:
                 if first:
                     
                     print("Move")
-                    time.sleep(2)
+                    time.sleep(0)
                     data=self.q.recv()
                     first=False
                 for i in data_dict:
                     data_dict[i].append(data[i][0][2])
-                if len(data_dict["F3"])==16:
+                if len(data_dict["F3"])==24:
                     for i in data_dict:
                         del data_dict[i][0]
                     #print(self.livedataclass.test_classifiers_ret(data_dict))
@@ -145,7 +145,7 @@ class LiveClassifierApplication:
                 #print(data)
                 for i in data_dict:
                     data_dict[i].append(data[i][0][2])
-                if len(data_dict["F3"])==16:
+                if len(data_dict["F3"])==24:
                     for i in data_dict:
                         del data_dict[i][0]
                     jj=self.livedataclass.test_classifiers_ret(data_dict)
@@ -160,19 +160,99 @@ class LiveClassifierApplication:
             self.getter.terminate()
             self.processer.terminate()
             raise
+def classify_func(arg):
+     return (arg[0], arg[1].test_classifiers_ret(arg[2]))
+def setup_classifers(q,doubles):
+     a=MultiClassifier(doubles,q)
+     a.RunSystem()
+     
+class MultiClassifier:
+     def __init__(self, doubles,q):
+          self.classifiers=dict()
+          self.q=q
+          for i in doubles:
+               curr=SLICERZ.LiveRunClassifier()
+               curr.run_train(i[1])
+               self.classifiers[i[0]]=curr
+     def RunSystem(self):
+          while True:
+               curr=self.q.recv()
+               results_vec=[]
+               for i in self.classifiers:
+                    results_vec.append(tuple((i, self.classifiers[i].test_classifiers_ret(curr))))
+               self.q.send(results_vec)
+               
+               
+               
+          
 class MultiLiveClassifierApplication:
-    def __init__(self, process1, process2, q,  profile):
+    def __init__(self, process1, process2, q,  profile,subprocessed=True):
         self.getter=process1
         self.profile=profile
         self.q=q
         self.dict_data=pickle.loads(profile.read())
         self.classifiers=dict()
+        if subprocessed:
+             dubs=[]
+             for i in self.dict_data:
+                  dubs.append(tuple((i, self.dict_data[i])))
+             self.classq, q=multiprocessing.Pipe()
+             self.classproc=multiprocessing.Process(target=setup_classifers, args=(q, dubs,))     
         for i in self.dict_data:
             curr_class=SLICERZ.LiveRunClassifier()
             curr_class.run_train(self.dict_data[i])
             self.classifiers[i]=curr_class
         self.processer=process2
         self.system_up_time=0
+
+    def runAppSubprocessed(self):
+        self.getter.start()
+        getpid=self.getter.pid
+        procget=kernel.OpenProcess(ctypes.c_uint(0x0200|0x0400), ctypes.c_bool(False), ctypes.c_uint(getpid))
+        kernel.SetPriorityClass(procget, 0x0100)
+        self.classproc.start()
+        classpid=self.getter.pid
+        proclass=kernel.OpenProcess(ctypes.c_uint(0x0200|0x0400), ctypes.c_bool(False), ctypes.c_uint(classpid))
+        kernel.SetPriorityClass(proclass, 0x0100)
+        self.processer.start()
+        procpid=self.processer.pid
+        procproc=kernel.OpenProcess(ctypes.c_uint(0x0200|0x0400), ctypes.c_bool(False), ctypes.c_uint(procpid))
+        kernel.SetPriorityClass(procproc, 0x0100)
+        data_dict=dict({"F3":[], "F4":[], "T7":[], "T8":[]})
+        countr=0
+        try:
+            while self.getter.is_alive():
+                data=self.q.recv()
+                #print(data)
+                for i in data_dict:
+                    data_dict[i].append(data[i][0][2])
+                countr=countr+1
+                if len(data_dict["F3"])==32:
+                    #print("In Detector")
+                    for i in data_dict:
+                        del data_dict[i][0]
+                    #if (countr%4)==0:
+                    self.classq.send(data_dict)
+                    res=self.classq.recv()
+                    #p=multiprocessing.Pool()
+                    
+                   # res=map(classify_func,res)
+                    #print(list(res))
+                    res1=list(res)
+                    print (len(res1))
+                    if max(res, key=lambda x:x[1])[1]>=0.85 and not max(res, key=lambda x:x[1])[0]=="neutral":
+                         print(str(max(res, key=lambda x:x[1])[0])+str(max(res, key=lambda x:x[1])))
+                    #countr=0
+                    #print(data_dict)
+                    
+                print(time.time())
+                self.system_up_time+=16/128
+        except:
+            self.getter.terminate()
+            self.classproc.terminate()
+            self.processer.terminate()
+            raise
+
     def runApp(self):
         self.getter.start()
         getpid=self.getter.pid
@@ -197,6 +277,12 @@ class MultiLiveClassifierApplication:
                         del data_dict[i][0]
                     #if (countr%4)==0:
                     res=[(tr, self.classifiers[tr].test_classifiers_ret(data_dict)) for tr in self.classifiers]
+                    #p=multiprocessing.Pool()
+                    
+                   # res=map(classify_func,res)
+                    #print(list(res))
+                    res1=list(res)
+                    print (len(res1))
                     if max(res, key=lambda x:x[1])[1]>=0.85 and not max(res, key=lambda x:x[1])[0]=="neutral":
                          print(str(max(res, key=lambda x:x[1])[0])+str(max(res, key=lambda x:x[1])))
                     #countr=0
@@ -243,7 +329,7 @@ def MultiRunApp():
     #print("Start")
     myApp=MultiLiveClassifierApplication(getter, processor, q3, open("C:/Users/Gaurav/Desktop/KineticEEGProgamFiles/Trainingdata.dat", "rb"))
     #myApp=LiveTrainingDataGatherer(getter, processor, q3, open("C:/Users/Gaurav/Desktop/KineticEEGProgamFiles/Trainingdata.dat", "wb"))
-    myApp.runApp()
+    myApp.runAppSubprocessed()
 if __name__=='__main__':
 ##    q,q1=multiprocessing.Pipe()
 ##    getter=multiprocessing.Process(target=BaseEEG.run_data_getter_processer, args=(q1,))
@@ -253,8 +339,8 @@ if __name__=='__main__':
 ##    myApp=LiveTrainingDataGatherer(getter, processor, q3, open("C:/Users/Gaurav/Desktop/KineticEEGProgamFiles/Trainingdata.dat", "wb"))
 ##    myApp.runApp()
      try:
-         DataGather()
-         RunApp()
+         MultiDataGather()
+         MultiRunApp()
      except Exception as e:
           print(e)
           input()
